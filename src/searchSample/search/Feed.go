f995aa7f7415861250de136f7023dcf26f857d54
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-04-14 12:09:25 +0200

    add search example

---
+++ src/searchSample/search/Feed.go
@@ -0,0 +1,31 @@
+package search
+
+import (
+	"encoding/json"
+	"os"
+)
+
+const dataFile = "src/searchSample/data/data.json"
+
+type Feed struct {
+	Name string `json:"site"`
+	URI string `json:"link"`
+	Type string `json:"type"`
+}
+
+
+func RetrieveFeeds() ([]*Feed, error) {
+
+	file, err := os.Open(dataFile)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	var feeds []*Feed
+	err = json.NewDecoder(file).Decode(&feeds)
+
+	return feeds, err
+}
